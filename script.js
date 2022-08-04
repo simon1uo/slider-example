97803b3330704f2539152a0e49d38579ab8aa30a
Simon Luo <simonlaw910@gmail.com>
2022-08-04 14:26:53 +0800

    feat: abstract normal model and mode config

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-class Slider {
-  constructor(id) {
+class Component {
+  constructor(
+    id,
+    options = {
+      name: "",
+      data: [],
+    }
+  ) {
     this.container = document.getElementById(id);
+    this.options = options;
+    this.container.innerHTML = this.render(options.data);
+  }
+
+  registerPlugins(...plugins) {
+    plugins.forEach((plugin) => {
+      const pluginContainer = document.createElement("div");
+      pluginContainer.className = `.${this.options.name}__plugin`;
+      pluginContainer.innerHTML = plugin.render(this.options.data);
+      this.container.appendChild(pluginContainer);
+      plugin.action(this);
+    });
+  }
+
+  render(data) {
+    // abstract
+    return "";
+  }
+}
+class Slider extends Component {
+  constructor(
+    id,
+    options = {
+      name: "slider-list",
+      data: [],
+      cycle: 3000,
+    }
+  ) {
+    super(id, options);
+
     this.items = this.container.querySelectorAll(
       ".slider-list__item, .slider-list__item--selected"
     );
+    this.cycle = options.cycle || 3000;
+    this.slideTo(0);
+  }
+
+  render(data) {
+    const content = data.map((image) =>
+      `<li class="slider-list__item">
+        <img src="${image}"/>
+      </li>`.trim()
+    );
+    return `<ul>${content.join("")}</ul>`;
   }
 
   getSelectedItem() {
@@ -55,62 +102,97 @@ class Slider {
   stop() {
     clearInterval(this._timer);
   }
-
-  registerPlugins(...plugins) {
-    plugins.forEach((plugin) => plugin(this));
-  }
 }
 
-const pluginController = (slider) => {
-  const controller = slider.container.querySelector(".slider-list__control");
-  const controlBtns = slider.container.querySelectorAll(
-    ".slider-list__control-buttons, .slider-list__control-buttons--selected"
-  );
+const pluginController = {
+  render(images) {
+    return `<div class="slider-list__control">
+      ${images
+        .map(
+          (image, i) =>
+            `<span class="slider-list__control-buttons${
+              i === 0 ? "--selected" : ""
+            }"></span>`
+        )
+        .join("")}
+    </div>`.trim();
+  },
 
-  controller.addEventListener("mouseover", (event) => {
-    const index = Array.from(controlBtns).indexOf(event.target);
-    if (index >= 0) {
-      slider.slideTo(index);
+  action(slider) {
+    const controller = slider.container.querySelector(".slider-list__control");
+    if (controller) {
+      const controlBtns = slider.container.querySelectorAll(
+        ".slider-list__control-buttons, .slider-list__control-buttons--selected"
+      );
+
+      controller.addEventListener("mouseover", (event) => {
+        const index = Array.from(controlBtns).indexOf(event.target);
+        if (index >= 0) {
+          slider.slideTo(index);
+          slider.stop();
+        }
+      });
+
+      controller.addEventListener("mouseout", (event) => {
+        slider.start();
+      });
+
+      slider.container.addEventListener("slide", (event) => {
+        const index = event.detail.index;
+        const selected = controller.querySelector(
+          ".slider-list__control-buttons--selected"
+        );
+        if (selected) {
+          selected.className = "slider-list__control-buttons";
+          controlBtns[index].className =
+            "slider-list__control-buttons--selected";
+        }
+      });
+    }
+  },
+};
+
+const pluginPrevious = {
+  render() {
+    return `<button class="slider-btn" id="slider-btn__prev">⬅️</button>`;
+  },
+  action(slider) {
+    const prevBtn = slider.container.querySelector("#slider-btn__prev");
+
+    prevBtn.addEventListener("click", () => {
       slider.stop();
-    }
-  });
-
-  controller.addEventListener("mouseout", () => {
-    slider.start();
-  });
-
-  slider.container.addEventListener("slide", (event) => {
-    const index = event.detail.index;
-    const selected = controller.querySelector(
-      ".slider-list__control-buttons--selected"
-    );
-    if (selected) {
-      selected.className = "slider-list__control-buttons";
-      controlBtns[index].className = "slider-list__control-buttons--selected";
-    }
-  });
+      slider.slidePrevious();
+      slider.start();
+      event.preventDefault();
+    });
+  },
 };
 
-const pluginPrevious = (slider) => {
-  const prevBtn = document.querySelector("#slider-btn__prev");
-  prevBtn.addEventListener("click", (event) => {
-    slider.stop();
-    slider.slidePrevious();
-    slider.start();
-    event.preventDefault();
-  });
+const pluginNext = {
+  render() {
+    return `<button class="slider-btn" id="slider-btn__next">➡️</button>`;
+  },
+  action(slider) {
+    const nextBtn = slider.container.querySelector("#slider-btn__next");
+
+    nextBtn.addEventListener("click", (event) => {
+      slider.stop();
+      slider.slideNext();
+      slider.start();
+      event.preventDefault();
+    });
+  },
 };
 
-const pluginNext = (slider) => {
-  const nextBtn = document.querySelector("#slider-btn__next");
-  nextBtn.addEventListener("click", (event) => {
-    slider.stop();
-    slider.slideNext();
-    slider.start();
-    event.preventDefault();
-  });
-};
-
-const slider = new Slider("my-slider");
-slider.registerPlugins(pluginController, pluginPrevious, pluginNext);
+const slider = new Slider("my-slider", {
+  name: "slider-list",
+  data: [
+    "https://source.unsplash.com/random/500x500/?ocean",
+    "https://source.unsplash.com/random/500x500/?sky",
+    "https://source.unsplash.com/random/500x500/?dog",
+    "https://source.unsplash.com/random/500x500/?cat",
+  ],
+  cycle: 2000,
+});
+slider.registerPlugins(pluginController, pluginNext, pluginPrevious);
 slider.start();
